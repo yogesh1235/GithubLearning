cad18ca139dc6d797e29ff63576d95ef1ffb714a
yogesh1235 <yogeshh.aggarwal@gmail.com>
2016-11-30 11:30:33 +0530

    file commiting

---
+++ TestingAnt/src/TestingAnt/Testant.java
@@ -0,0 +1,17 @@
+package TestingAnt;
+
+import org.testng.annotations.Test;
+
+
+
+public class Testant {
+	@Test
+	public  void king (){
+		
+		System.out.println("This is a test method");
+		
+		
+	}
+	
+	
+}
